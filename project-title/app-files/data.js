8e0d95479a7731585d54ce15b92c2eb92a171881
Charlton Luebstorff <cluebstorff@mac-cluebstorff-lt.JHCS.LOC>
2025-10-30 21:27:11 -0400

    init_test

---
+++ project-title/app-files/data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20251030_194401_00_046",
+      "name": "IMG_20251030_194401_00_046",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.9947721431584693,
+        "pitch": 0.06996305416982018,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0064808215321115,
+          "pitch": 0.24160382274365944,
+          "rotation": 0,
+          "target": "1-img_20251030_194439_00_047"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_20251030_194439_00_047",
+      "name": "IMG_20251030_194439_00_047",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.0603227816442935,
+        "pitch": 0.1000639982562248,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03766581722326201,
+          "pitch": 0.24244069637979493,
+          "rotation": 0,
+          "target": "0-img_20251030_194401_00_046"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
